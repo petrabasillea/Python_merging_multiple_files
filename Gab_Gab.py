8e546e525217172463cfa3e8ad9457d3818aa742
petrabasillea <petra.basillea@gmail.com>
2022-08-23 16:57:48 +0700

    ok

--- Gab_Gab.py
+++ Gab_Gab.py
@@ -1,25 +1,37 @@
 import os
 import csv
+import datetime
 
-directory_name = '.\Raw'
-filewrite = "outoput.csv"
+dt = datetime.datetime.now()
+dtstr = dt.strftime("%Y-%m-%w_%H.%M.%S")
+
+directory_name  = '.\Raw'
+filewrite       = "output_"+dtstr+".csv"
+x = 1
 
 
 for file_name in os.listdir(directory_name):
     i = os.path.join(directory_name, file_name)
     if os.path.isfile(i):
-        print(i)
-        fileread = i
+        print("File ke-",x," : ", file_name)
+
+        fileread = i        
 
         with open(fileread) as file_obj:
-
-            heading = next(file_obj)
-            reader_obj = csv.reader(file_obj)
+            
+            if x>1:
+                heading = next(file_obj)
+                reader_obj = csv.reader(file_obj)
+            else:
+                #heading = next(file_obj)
+                reader_obj = csv.reader(file_obj)
 
             for row in reader_obj:
-                #print(row)
                 data = [row] 
         
                 with open(filewrite, 'a+', newline ='') as file:   
                         csvwriter = csv.writer(file)
                         csvwriter.writerows(data)
+        
+        x = x+1
+        
