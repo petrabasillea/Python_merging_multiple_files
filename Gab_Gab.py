ef2bfa361ddb736ecbad2e6a2a19d784c529d640
petrabasillea <petra.basillea@gmail.com>
2022-08-23 16:06:48 +0700

    ok

--- Gab_Gab.py
+++ Gab_Gab.py
@@ -1,2 +1,25 @@
+import os
 import csv
 
+directory_name = '.\Raw'
+filewrite = "outoput.csv"
+
+
+for file_name in os.listdir(directory_name):
+    i = os.path.join(directory_name, file_name)
+    if os.path.isfile(i):
+        print(i)
+        fileread = i
+
+        with open(fileread) as file_obj:
+
+            heading = next(file_obj)
+            reader_obj = csv.reader(file_obj)
+
+            for row in reader_obj:
+                #print(row)
+                data = [row] 
+        
+                with open(filewrite, 'a+', newline ='') as file:   
+                        csvwriter = csv.writer(file)
+                        csvwriter.writerows(data)
